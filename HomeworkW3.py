d545c558dc54df115ee37ce837a14827cbdbcb19
rachelmisty <86662345+rachelmisty@users.noreply.github.com>
2021-08-06 17:16:25 +0100

    adding homework week3

---
+++ HomeworkW3.py
@@ -0,0 +1,78 @@
+"""
+Create required phrase.
+----------------------
+
+You are given a string of available characters and a string representing a word or a phrase that you need to generate.
+Write a function that checks if you cab generate required word/phrase using the characters provided.
+If you can, then please return True, otherwise return False.
+
+NOTES:
+    You can only generate the phrase if the frequency of unique characters in the characters string is equal or greater
+    than frequency in the document string.
+
+FOR EXAMPLE:
+
+    characters = "cbacba"
+    phrase = "aabbccc"
+
+    In this case you CANNOT create required phrase, because you are 1 character short!
+
+IMPORTANT:
+    The phrase you need to create can contain any characters including special characters, capital letter, numbers
+    and spaces.
+
+    You can always generate an empty string.
+
+"""
+
+import itertools
+
+characters = "cbacbac"
+phrase = "aabbccc"
+
+def generate_phrase(characters, phrase):
+    if len(characters) < len(phrase):
+        return False
+
+    for p in itertools.permutations(characters):
+        if ''.join(p).__contains__(phrase):
+            return True
+
+    return False
+
+print(generate_phrase(characters, phrase))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
